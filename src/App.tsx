777d5848a8a4e6506f09d50b8d249087e32d4311
Luvkitri <janjedrzejewski@gmail.com>
2020-12-14 21:10:38 +0100

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // containers
-import Home from './containers/Home'
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 // components
 import Header from './components/Header';
@@ -12,9 +15,14 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 const App: React.FC = () => {
     return (
         <div className="App">
-            <CssBaseline />
-            <Header />
-            <Home />
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/users/login" exact component={Login}/>
+                    <Route path="/users/signup" exact component={Signup} />
+                    <Route path="/" render={() => <div><h1>404</h1></div>} />
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
