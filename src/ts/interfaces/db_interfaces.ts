b71c09a617ddf59422c9e6a61a689dc9e259d75e
Luvkitri <janjedrzejewski@gmail.com>
2020-12-15 02:37:45 +0100

    Added user interface

--- src/ts/interfaces/db_interfaces.ts
+++ src/ts/interfaces/db_interfaces.ts
@@ -37,3 +37,14 @@ export interface SubThread {
     name: string;
     thread_id: number;
 }
+
+export interface User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    login: string;
+    password: string;
+    updated_at: string;
+    created_at: string;
+}
