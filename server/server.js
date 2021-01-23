73e2cac81b03ed2b019be9227aeba1e93c10be2e
Luvkitri <janjedrzejewski@gmail.com>
2021-01-23 04:08:09 +0100

    Deployment server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
+
+const publicPath = path.join(__dirname, '..', 'public');
+const buildPath = path.join(__dirname, '..', 'build');
+const rootPath = path.join(__dirname, '..');
+const port = process.env.PORT || 3000;
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(rootPath));
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('App is running in production mode');
+});
