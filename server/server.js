b31d9de35bfb5afe8d6865b7e34f82f8db9bd067
Luvkitri <janjedrzejewski@gmail.com>
2021-01-23 04:22:59 +0100

    Paths fixed

--- server/server.js
+++ server/server.js
@@ -5,12 +5,15 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
 const buildPath = path.join(__dirname, '..', 'build');
+const faviconPath = path.join(__dirname, '..', 'build', 'favicon.ico');
 const rootPath = path.join(__dirname, '..');
 const port = process.env.PORT || 3000;
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(faviconPath));
 app.use(express.static(rootPath));
 app.use(express.static(buildPath));
+
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
